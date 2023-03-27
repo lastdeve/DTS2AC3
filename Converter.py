5712ceb4f5da5df9743e2d7857a1914cb230396b
lastdeve <hampus.staaf02@gmail.com>
2023-03-27 22:08:16 +0200

    Big update

--- Converter.py
+++ Converter.py
@@ -1,85 +1,109 @@
-# Importing modules
-import sys
 import os
+import sys
 import subprocess
-from PySide6.QtWidgets import QApplication, QWidget, QFileDialog, QPushButton, QLabel, QGridLayout
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QMessageBox
+from PyQt5.QtCore import Qt, QThread, pyqtSignal
+from Ui_DTS2AC3 import Ui_MainWindow
+os.environ['QT_AUTO_SCREEN_SCALE_FACTOR'] = '1'
 
-# Creating a custom widget class
-class Converter(QWidget):
+
+class DTS2AC3Converter(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self.setupUi(self)
+        self.progressBar.setValue(0)
+        self.pushButton.clicked.connect(self.convert)
+        self.pushButton_2.clicked.connect(self.select_input_file)
+        self.pushButton_3.clicked.connect(self.select_output_dir)
 
-    def initUI(self):
-        # Setting the window title and size
-        self.setWindowTitle("MKV DTS to AC3 Converter")
-        self.resize(400, 200)
+    def select_input_file(self):
+        file_path, _ = QFileDialog.getOpenFileName(self, "Select Input MKV File", "", "MKV Files (*.mkv)")
+        if file_path:
+            self.lineEdit.setText(file_path)
 
-        # Creating a grid layout
-        layout = QGridLayout()
+    def select_output_dir(self):
+        output_dir = QFileDialog.getExistingDirectory(self, "Select Output Directory", "")
+        if output_dir:
+            self.lineEdit_2.setText(output_dir)
 
-        # Creating a label for the input file
-        self.input_label = QLabel("Input file: None")
-        layout.addWidget(self.input_label, 0, 0)
+    def convert(self):
+        input_file = self.lineEdit.text()
+        output_dir = self.lineEdit_2.text()
 
-        # Creating a button for choosing the input file
-        self.input_button = QPushButton("Choose input file")
-        self.input_button.clicked.connect(self.choose_input_file)
-        layout.addWidget(self.input_button, 0, 1)
+        if not input_file:
+            self.show_message_box("Please select input file.")
+            return
 
-        # Creating a label for the output file
-        self.output_label = QLabel("Output: None")
-        layout.addWidget(self.output_label, 1, 0)
+        if not output_dir:
+            self.show_message_box("Please select output directory.")
+            return
 
-        # Creating a button for choosing the output file
-        self.output_button = QPushButton("Choose output")
-        self.output_button.clicked.connect(self.choose_output_file)
-        layout.addWidget(self.output_button, 1, 1)
+        output_file = os.path.join(output_dir, os.path.basename(input_file))
 
-        # Creating a button for converting the file
-        self.convert_button = QPushButton("Convert")
-        self.convert_button.clicked.connect(self.convert_file)
-        layout.addWidget(self.convert_button, 2, 0, 1, 2)
+        command = f"ffmpeg -i \"{input_file}\" -c:v copy -c:a ac3 \"{output_file}\""
+        self.run_command(command)
 
-        # Setting the layout for the widget
-        self.setLayout(layout)
+    def run_command(self, command):
+        self.thread = FFmpegThread(command)
+        self.thread.progress.connect(self.update_progress)
+        self.thread.finished.connect(self.on_finished)
+        self.thread.start()
 
-    def choose_input_file(self):
-        # Opening a file dialog for choosing the input file
-        filename, _ = QFileDialog.getOpenFileName(self, "Open input file", "", "MKV files (*.mkv)")
-        
-        # Checking if a valid file was chosen
-        if filename:
-            # Updating the input label and storing the filename
-            self.input_label.setText(f"Input file: {filename}")
-            self.input_filename = filename
+    def update_progress(self, value):
+        self.progressBar.setValue(value)
 
-    def choose_output_file(self):
-        # Opening a file dialog for choosing the output file
-        filename, _ = QFileDialog.getSaveFileName(self, "Save output file", "", "MKV files (*.mkv)")
+    def on_finished(self):
+        self.progressBar.setValue(100)
+        self.show_message_box("Conversion completed successfully.")
 
-        # Checking if a valid file was chosen
-        if filename:
-            # Updating the output label and storing the filename
-            self.output_label.setText(f"Output file: {filename}")
-            self.output_filename = filename
+    def show_message_box(self, message):
+        QMessageBox.information(self, "DTS2AC3", message)
 
-    def convert_file(self):
-        # Checking if both input and output files are set
-        if hasattr(self, "input_filename") and hasattr(self, "output_filename"):
-            # Running the ffmpeg command with subprocess
-            command = ["ffmpeg", "-i", self.input_filename, "-c:v", "copy", "-c:a", "ac3", self.output_filename]
-            subprocess.run(command)
-            
-            # Showing a message that the conversion is done
-            self.convert_button.setText("Done!")
-        
-# Creating an application instance
-app = QApplication(sys.argv)
 
-# Creating and showing the converter widget
-converter = Converter()
-converter.show()
+class FFmpegThread(QThread):
+    progress = pyqtSignal(int)
 
-# Starting the main loop
-sys.exit(app.exec_())
+    def __init__(self, command):
+        super().__init__()
+        self.command = command
+
+    def run(self):
+        process = subprocess.Popen(
+            self.command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True,
+        )
+
+        duration_seconds = None
+
+        while process.poll() is None:
+            line = process.stderr.readline().strip()
+            if "Duration:" in line:
+                duration = line.split("Duration:")[1].strip().split(",")[0]
+                duration_seconds = self.time_to_seconds(duration)
+
+            if "time=" in line:
+                time = line.split("time=")[1].strip().split()[0]
+                time_seconds = self.time_to_seconds(time)
+                progress = int(time_seconds / duration_seconds * 100)
+                self.progress.emit(progress)
+
+        # read any remaining output
+        for line in process.stderr:
+            pass
+
+        process.wait()
+
+    def time_to_seconds(self, time_str):
+        hours, minutes, seconds = time_str.split(":")
+        return int(hours) * 3600 + int(minutes) * 60 + float(seconds)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    converter = DTS2AC3Converter()
+    converter.show()
+    sys.exit(app.exec_())
